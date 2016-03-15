dae9f087235a7be0fad801ba1f9e276e7c19d8bb
Алексей <grinderbrader@gmail.com>
2016-03-15 10:21:43 -0500

    installing extensions

---
+++ naideno.Core/Entities/OtherThings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naideno.Core.Entities
+{
+    class OtherThings
+    {
+        public int Name { get; set; }
+        public int Descriprion { get; set; }
+        public int UploadDate { get; set; }
+        public int HumID { get; set; }
+    }
+}
